b744eca263674f3b18533e8ee8c32ec2d87d93fd
anand <anandparadkar8@gmail.com>
2024-01-29 18:44:36 +0530

    added spec helper

---
+++ spec/features/rails_server_spec.rb
@@ -0,0 +1,11 @@
+
+require 'rails_helper'
+
+RSpec.feature 'Welcome Page' do
+  scenario 'renders the default Rails welcome page' do
+    # Visit the root path
+    visit '/'
+
+    # Add assertions based on the content of your welcome page
+  end
+end
